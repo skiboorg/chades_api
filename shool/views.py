197c0ac3647e571206ac9082249b947871586ee2
skiboorg <d@skib.org>
2021-12-20 18:57:44 +0300

    add celery

--- shool/views.py
+++ shool/views.py
@@ -138,91 +138,91 @@ class GetInputTests(generics.ListAPIView):
     GetInputTests = InputTest.objects.all()
     serializer_class = InputTestSerializer
 
-import os
-import logging
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-    filemode='a',)
-logger = logging.getLogger('')
-
-
-APPID = '2021002136604704'
-notify_url = 'http://qrlevel.cn/api/v1/shool/pay_notify/'
-APIPAY = "https://openapi.alipaydev.com/gateway.do?"
-
-DEBUG=True
-
-BASE_DIR = os.getcwd()
+# import os
+# import logging
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s %(levelname)s %(message)s',
+#     filemode='a',)
+# logger = logging.getLogger('')
+#
+#
 # APPID = '2021002136604704'
-private_key = open(os.path.join(BASE_DIR,'private_key.txt')).read()
-public_key  = open(os.path.join(BASE_DIR,'public_key.txt')).read()
-APIPAY = "https://openapi.alipay.com/gateway.do?"
-
-from alipay import AliPay
-
-import time
-# import Response
-
-def alipay():  # 之前是 pay_result
-    ap = AliPay(
-        appid=APPID,
-        app_notify_url=notify_url,  # 默认回调url
-        app_private_key_string=private_key,
-        alipay_public_key_string=public_key,  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
-        sign_type="RSA2",  # RSA 或者 RSA2
-        debug=DEBUG  # 默认False ,若开启则使用沙盒环境的支付宝公钥
-    )
-
-    return ap
-
-def pay(ap,amount,subject,out_trade_no):
-    order_string = ap.api_alipay_trade_page_pay(
-        total_amount=amount,  # 价格
-        subject=subject,  # 主题
-        out_trade_no=out_trade_no  # 订单ID
-    )
-    pay_url = APIPAY + order_string  # 调用支付宝支付接口
-    return pay_url
-
-class CreatePay(APIView):
-    def post(self,request):
-        print(request.data)
-        period = int(request.data.get('period'))
-        subject = f'User {request.data.get("user_id")} payment {str(time.time()).replace(",", "")}'
-        out_trade_no = f'qrlevel_reg_{period}_user_{request.data.get("user_id")}_{str(time.time()).replace(",", "")}'
-        amount = 0
-
-        if period == 1:
-            amount = 10
-
-        if period == 2:
-            amount = 20
-
-        if period == 3:
-            amount = 30
-
-        ap = alipay()
-        url = pay(ap,amount=amount,subject=subject,out_trade_no=out_trade_no)
-        return Response(url, status=200)
-
-class PayNotify(APIView):
-    def post(self, request):
-        print('post')
-
-        out_trade_no = request.data.get('out_trade_no')
-        trade_status = request.data.get('trade_status')
-        # print(out_trade_no)
-        # print(trade_status)
-        # print(out_trade_no.split('_'))
-        months = int(out_trade_no.split('_')[2])
-        user_id = out_trade_no.split('_')[4]
-        if trade_status == 'TRADE_SUCCESS':
-            user = User.objects.get(id=user_id)
-            # print(user)
-            # print(datetime.today())
-            # print(datetime.today()+ relativedelta(months=months))
-            user.expiry_time = datetime.today()+ relativedelta(months=months)
-            user.save()
-        return Response(status=200)
-
+# notify_url = 'http://qrlevel.cn/api/v1/shool/pay_notify/'
+# APIPAY = "https://openapi.alipaydev.com/gateway.do?"
+#
+# DEBUG=True
+#
+# BASE_DIR = os.getcwd()
+# # APPID = '2021002136604704'
+# private_key = open(os.path.join(BASE_DIR,'private_key.txt')).read()
+# public_key  = open(os.path.join(BASE_DIR,'public_key.txt')).read()
+# APIPAY = "https://openapi.alipay.com/gateway.do?"
+#
+# from alipay import AliPay
+#
+# import time
+# # import Response
+#
+# def alipay():  # 之前是 pay_result
+#     ap = AliPay(
+#         appid=APPID,
+#         app_notify_url=notify_url,  # 默认回调url
+#         app_private_key_string=private_key,
+#         alipay_public_key_string=public_key,  # 支付宝的公钥，验证支付宝回传消息使用，不是你自己的公钥,
+#         sign_type="RSA2",  # RSA 或者 RSA2
+#         debug=DEBUG  # 默认False ,若开启则使用沙盒环境的支付宝公钥
+#     )
+#
+#     return ap
+#
+# def pay(ap,amount,subject,out_trade_no):
+#     order_string = ap.api_alipay_trade_page_pay(
+#         total_amount=amount,  # 价格
+#         subject=subject,  # 主题
+#         out_trade_no=out_trade_no  # 订单ID
+#     )
+#     pay_url = APIPAY + order_string  # 调用支付宝支付接口
+#     return pay_url
+#
+# class CreatePay(APIView):
+#     def post(self,request):
+#         print(request.data)
+#         period = int(request.data.get('period'))
+#         subject = f'User {request.data.get("user_id")} payment {str(time.time()).replace(",", "")}'
+#         out_trade_no = f'qrlevel_reg_{period}_user_{request.data.get("user_id")}_{str(time.time()).replace(",", "")}'
+#         amount = 0
+#
+#         if period == 1:
+#             amount = 10
+#
+#         if period == 2:
+#             amount = 20
+#
+#         if period == 3:
+#             amount = 30
+#
+#         ap = alipay()
+#         url = pay(ap,amount=amount,subject=subject,out_trade_no=out_trade_no)
+#         return Response(url, status=200)
+#
+# class PayNotify(APIView):
+#     def post(self, request):
+#         print('post')
+#
+#         out_trade_no = request.data.get('out_trade_no')
+#         trade_status = request.data.get('trade_status')
+#         # print(out_trade_no)
+#         # print(trade_status)
+#         # print(out_trade_no.split('_'))
+#         months = int(out_trade_no.split('_')[2])
+#         user_id = out_trade_no.split('_')[4]
+#         if trade_status == 'TRADE_SUCCESS':
+#             user = User.objects.get(id=user_id)
+#             # print(user)
+#             # print(datetime.today())
+#             # print(datetime.today()+ relativedelta(months=months))
+#             user.expiry_time = datetime.today()+ relativedelta(months=months)
+#             user.save()
+#         return Response(status=200)
+#
